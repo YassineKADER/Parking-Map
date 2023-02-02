c949d62ca3f4583f0e22fad14e36f2effdeeae1f
EROS <yassine.kader2017fc@gmail.com>
2023-02-02 14:12:50 +0100

    fix map size

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,14 +1,16 @@
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, Popup, TileLayer, ZoomControl } from "react-leaflet";
 import "./Map.css"
-import "leaflet/dist/leaflet.css"
+
+
 
 export default function Map() {
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer center={[51.505, -0.09]} zoom={8} scrollWheelZoom={true} zoomControl={false} >
       <TileLayer
         attribution='&copy; <a href="https:/rg/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      <ZoomControl position="bottomright"/>
       <Marker position={[51.505, -0.09]}>
         <Popup>
           A pretty C3 popup. <br /> Easily customizable.
