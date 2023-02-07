5f4fa5da589484b997b9ea62706cb879cfec42b2
EROS <yassine.kader2017fc@gmail.com>
2023-02-07 17:30:09 +0100

    search bar function

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -5,11 +5,32 @@ import { useMap } from "react-leaflet";
 
 
 export default function Searchbar(props){
-    function searchParking(infos){
+    const map = useMap();
+    function searchParking(event){
+        event.preventDefault();
+        let index=false;
+        name = document.getElementById("query").value;
+        props.parkingsinfo.forEach((parking)=>{
+            if(parking.name.toLowerCase().includes(name.toLowerCase()) || parking.location.city.toLowerCase().includes(name.toLowerCase())){
+                if(parking.freespace>0){
+                    map.setZoom(9)
+                    map.flyTo([parking.location.latitude,parking.location.longitude]);
+                    index=true;
+                    return;
+                }
+            }
+        })
+        if(index==false){
+            document.getElementById("message").className="show";
+                setTimeout(()=>{
+                    document.getElementById("message").className="hide";
+                }, 1500)
+                
+        }
     }
     return (<form id="form" role="search" onSubmit={searchParking}>
     <input type="search" id="query" name="q" placeholder="Search for city, or parking name" aria-label="Search Throw Parkings Positions"/>
     <button type="submit" id="submit"><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
-    
+    <p id="message" className="hide">No parkings found on this city</p>
     </form>)
 }
