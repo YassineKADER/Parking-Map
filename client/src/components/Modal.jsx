a458876f517bbfc580d19fd283a0bed0c7bc1d7c
EROS <yassine.kader2017fc@gmail.com>
2023-02-11 18:06:55 +0100

    commit

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -30,9 +30,11 @@ export default function Modal({onClose, open, data, location}){
         <div className="topModal"><p><FontAwesomeIcon icon={faMapPin} color={(data.freespace>0)?"green":"red"}/> {data.name}</p><button onClick={onClose}><FontAwesomeIcon icon={faXmark} /></button></div>
             <div className="modalContent" id="modalContent">
             <p>{data.description}</p>
-            <span><FontAwesomeIcon icon={faCar} /><p> Avalibale Spaces: {data.freespace}</p></span>
-            <span><FontAwesomeIcon icon={faMapLocationDot} /><p> Distance: {getDistanceFromLatLonInKm(location[0],location[1],data.location.latitude,data.location.longitude)}</p></span>
-            <span><FontAwesomeIcon icon={faStopwatch} /> <p>Time:</p></span>
+            <div className="dtcontainer">
+            <span><FontAwesomeIcon icon={faCar} /><p> Available Places: </p><span>{data.freespace}</span></span>
+            <span><FontAwesomeIcon icon={faMapLocationDot} /><p> Distance: </p><span>{getDistanceFromLatLonInKm(location[0],location[1],data.location.latitude,data.location.longitude).toFixed(2)}km</span></span>
+            <span><FontAwesomeIcon icon={faStopwatch} /> <p>Time: </p><span>{(getDistanceFromLatLonInKm(location[0],location[1],data.location.latitude,data.location.longitude)/104).toFixed(2)}h</span></span>
+            </div>
             <div><button onClick={showIngoogleMap}>Open in google Map</button></div>
             </div>
         </div>
