1ed67cb05a52d1890ea0b68beffc13134f6d8a75
EROS <yassine.kader2017fc@gmail.com>
2023-02-12 17:30:05 +0100

    Update ChangeRoute.jsx

--- client/src/components/ChangeRoute.jsx
+++ client/src/components/ChangeRoute.jsx
@@ -6,9 +6,10 @@ export default function Changeroute(props){
     const map = useMap();
     map.eachLayer((layer)=>{
             if(layer.options.waypoints && layer.options.waypoints.length){
-                console.log(layer.getWaypoints()[0]["latLng"])
-                layer.spliceWaypoints(0, 2);
-                layer.setWaypoints([L.latLng(props.lat, props.long), L.latLng(props.lat1, props.long1)])
+                if((layer.getWaypoints()[1]["latLng"]["lat"]!=props.lat1) && (layer.getWaypoints()[1]["latLng"]["lng"]!=props.long1)){
+                    layer.spliceWaypoints(0, 2);
+                    layer.setWaypoints([L.latLng(props.lat, props.long), L.latLng(props.lat1, props.long1)])    
+                }
             }
         })
 }
