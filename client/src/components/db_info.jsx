8f52c0c764efb7c168278ccd2d0d7ddbc3120466
EROS <80207770+YassineKADER@users.noreply.github.com>
2023-04-17 14:54:08 +0000

    Update db_info.jsx

--- client/src/components/db_info.jsx
+++ client/src/components/db_info.jsx
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyBgGx67w032_zncuZ37tFYPrm02rH1XbrY",
-  authDomain: "wise-baton-353710.firebaseapp.com",
-  databaseURL: "https://wise-baton-353710-default-rtdb.firebaseio.com",
-  projectId: "wise-baton-353710",
-  storageBucket: "wise-baton-353710.appspot.com",
-  messagingSenderId: "962857669223",
-  appId: "1:962857669223:web:3360987f13c2f1e6787ac2",
+  apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  authDomain: "xxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  databaseURL: "xxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  projectId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  storageBucket: "xxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  messagingSenderId: "xxxxxxxxxxxxx",
+  appId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
 };
 
 export default firebaseConfig
